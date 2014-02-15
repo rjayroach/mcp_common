a94835181577f76f0263648c6be52db92a38e7e7
Robert Roach <rjayroach@gmail.com>
2014-02-16 07:25:39 +0800

    fix tests [finsishes #65852892]

--- app/models/mcp_common/person.rb
+++ app/models/mcp_common/person.rb
@@ -1,5 +1,5 @@
 module McpCommon
   class Person < ActiveRecord::Base
-    attr_accessible :first_name, :last_name, :personable
+    #attr_accessible :first_name, :last_name, :personable
   end
 end
--- spec/models/mcp_common/person_spec.rb
+++ spec/models/mcp_common/person_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 module McpCommon
   describe Person do
-    pending "add some examples to (or delete) #{__FILE__}"
+    #pending "add some examples to (or delete) #{__FILE__}"
   end
 end
